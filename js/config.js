4a83a7d6ba7692de41c980922906887c1c2bd650
laerionrp <laerion.rp@gmail.com>
2026-07-04 14:49:01 +0200

    Fix config file

--- js/config.js
+++ js/config.js
@@ -2,7 +2,5 @@ const CONFIG = {
     APP_NAME: "Reroll Check Perf",
     VERSION: "1.0.0",
     API_URL: "https://script.google.com/macros/s/AKfycbyxe3gdftmcjV2GM02MneVoVKuB27YLVvAEg8CYlxifvtUf4ZPOoR6g2bHtHKmSdSa2JQ/exec",
-    APP_NAME: "Reroll Check Perf",
-    VERSION: "1.0.0"
     DEFAULT_THEME: "dark"
 };
