1487bc03b3e6d5181e73772b7bdf73846ca81294
laerionrp <laerion.rp@gmail.com>
2026-08-04 17:13:57 +0200

    fix: met à jour l URL Apps Script

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     APP_NAME: "Reroll Check Perf",
     VERSION: "v1.3.4",
-    API_URL: "https://script.google.com/macros/s/AKfycbyxe3gdftmcjV2GM02MneVoVKuB27YLVvAEg8CYlxifvtUf4ZPOoR6g2bHtHKmSdSa2JQ/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbzRg3eQWrDv3b7P8k0bIZl_jx5e5xYEO1Z91Ll8BZA4L8TII-kBQeohjAdLCb3Ot6ESLA/exec",
     DEFAULT_THEME: "dark"
 };
